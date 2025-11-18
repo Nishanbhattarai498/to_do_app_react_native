998f36dc0bff1b556814be8a9c0a8c9ee40148ae
nishan <bhattarainisham@gmail.com>
2025-11-18 10:15:57 +0545

    add navigation layout and about screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: "To-Do App",
+          headerTitleAlign: "center",
+        }}
+      />
+
+      <Stack.Screen
+        name="about"
+        options={{
+          headerTitle: "About",
+          headerTitleAlign: "center",
+        }}
+      />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { Background } from "@react-navigation/elements";
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={
+        styles.container
+      }
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.content}>Edit app/index.tsx to edit this screen hey heyS s.</Text>
+      <Link href="/about" style={{ marginTop: 20, fontSize: 18, color: 'blue' }}>
+
+      Visit about screen
+      
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+   
+  },
+  content: {
+    
+    fontSize: 20,
+  },
+});
